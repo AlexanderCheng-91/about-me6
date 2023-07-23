c7afafe8bd1ed7109b7f038cb8ed8b10438ee4b1
AlexanderCheng <achengster4@gmail.com>
2023-07-23 08:05:03 -0700

    removed a navbar because of dropdown menu error

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 //components
 import { AlexCard } from './components/alexCard';
 import { HeroGif } from './components/heroGif';
-import { ExampleNavBar } from './components/ExampleNavBar';
+// import { ExampleNavBar } from './components/ExampleNavBar';
 import { OldAboutMe } from './components/OldAboutMe'
 //about me component
 //blog component (want this feature to look like a num pad on desktop/ single image on screen)
@@ -29,11 +29,11 @@ function App() {
   return (
     
     <Router>
-      <ExampleNavBar></ExampleNavBar>
+      {/* <ExampleNavBar></ExampleNavBar> */}
       <Navbar></Navbar>
       <OldAboutMe/>
       <Routes>
-        <Route path='/' element={<Dogs/>} />
+        <Route path='/react-gh-pages' element={<Dogs/>} />
         <Route path='/cats' element={<Cats/>} /> 
         {/* <Route path='/sheeps' exact component={Sheeps}/> */}
         {/* <Route path='/goats' exact component={Goats}/> */}
