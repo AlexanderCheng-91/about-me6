42e5f28a5e3e210d460c60e7049e51b48e8b16cf
AlexanderCheng <achengster4@gmail.com>
2023-07-24 13:36:50 -0700

    comment out everything except dogs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 //components
-import { AlexCard } from './components/alexCard';
-import { HeroGif } from './components/heroGif';
+// import { AlexCard } from './components/alexCard';
+// import { HeroGif } from './components/heroGif';
 // import { ExampleNavBar } from './components/ExampleNavBar';
-import { OldAboutMe } from './components/OldAboutMe'
+// import { OldAboutMe } from './components/OldAboutMe'
 //about me component
 //blog component (want this feature to look like a num pad on desktop/ single image on screen)
 //skills component 
 //email me component
 
 //Router
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/NavbarComponent";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Navbar from "./components/NavbarComponent";
 import Dogs from './pages/Dogs';
-import Cats from './pages/Cats';
+// import Cats from './pages/Cats';
 // import Sheeps from './pages/Sheeps';
 // import Goats from './pages/Goats';
 
@@ -27,37 +27,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    
-    <Router>
-      {/* <ExampleNavBar></ExampleNavBar> */}
-      <Navbar></Navbar>
-      <OldAboutMe/>
-      <Routes>
-        <Route path='/react-gh-pages' element={<Dogs/>} />
-        <Route path='/cats' element={<Cats/>} /> 
-        {/* <Route path='/sheeps' exact component={Sheeps}/> */}
-        {/* <Route path='/goats' exact component={Goats}/> */}
-      </Routes> 
-      <div className="App">
-        <header className="App-header">
-          <HeroGif></HeroGif>
-          <AlexCard></AlexCard>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. 
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <Dogs/>
+    // <Router>
+    //   {/* <ExampleNavBar></ExampleNavBar> */}
+    //   <Navbar></Navbar>
+    //   <OldAboutMe/>
+    //   <Routes>
+    //     <Route path='/react-gh-pages' element={<Dogs/>} />
+    //     <Route path='/cats' element={<Cats/>} /> 
+    //     {/* <Route path='/sheeps' exact component={Sheeps}/> */}
+    //     {/* <Route path='/goats' exact component={Goats}/> */}
+    //   </Routes> 
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <HeroGif></HeroGif>
+    //       <AlexCard></AlexCard>
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <p>
+    //         Edit <code>src/App.js</code> and save to reload. 
+    //       </p>
+    //       <a
+    //         className="App-link"
+    //         href="https://reactjs.org"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         Learn React
+    //       </a>
+    //     </header>
+    //   </div>
       
-    </Router>
+    // </Router>
   );
 }
 
